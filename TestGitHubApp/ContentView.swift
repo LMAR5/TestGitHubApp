fbad54aa53b2481b196eb9ddf08204bb1f7e20ff
Luis Aguilar <luisaguilar159@gmail.com>
2023-11-12 14:44:51 -0800

    Adding a title to the list of items

--- TestGitHubApp/ContentView.swift
+++ TestGitHubApp/ContentView.swift
@@ -19,6 +19,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List {
+                Text("List of items").bold()
                 ForEach(items) { item in
                     NavigationLink {
                         Text("Item at \(item.timestamp!, formatter: itemFormatter)")
